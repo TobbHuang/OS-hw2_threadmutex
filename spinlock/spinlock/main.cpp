b2e342c195c3152b2c4284f4185afe7e90fe25cb
Tobb Huang <qaz589@qq.com>
2015-11-15 14:03:47 +0800

    spinlock update

--- spinlock/spinlock/main.cpp
+++ spinlock/spinlock/main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <spinlock.h>
 
 pthread_spinlock_t spinlock;
 
@@ -8,27 +7,30 @@ int value=0;
 
 void* ThreadRunner(void*){
     int k;
-    for(k=0;k<10000;k++){
+    for(k=0;k<100000000;k++){
+        pthread_spin_lock(&spinlock);
         value++;
+        pthread_spin_unlock(&spinlock);
         //printf("%d\n",value);
     }
     return 0;
 }
 
 int main(int argc, const char * argv[]) {
-    
-    
+
+    pthread_spin_init(&spinlock,0);
+
     pthread_t tid[3];
     int i;
     for(i=0;i<3;i++){
         pthread_create(&tid[i], NULL, ThreadRunner, 0);
     }
-    
+
     for(i=0;i<3;i++){
         pthread_join(tid[i], NULL);
     }
-    
+
     printf("end:%d\n",value);
-    
+
     return 0;
 }
